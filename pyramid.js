23860497bdcbaf2dc5551be237eccc5107bf6e14
sukmin <chltjrals5016@naver.com>
2023-03-29 14:46:03 +0900

    pyramid.js

---
+++ pyramid.js
@@ -0,0 +1,28 @@
+//피라미드
+for (let i = 0; i <= 5; i++) {
+  for (let j = 0; j < i; j++) {
+    // document.write("*");
+    process.stdout.write("*");
+  }
+  // document.write("<br>");
+  console.log("");
+}
+
+//피라미드 reverse
+for (let i = 0; i <= 5; i++) {
+  for (let j = 4; j > i; j--) {
+    // document.write("*");
+    process.stdout.write("*");
+  }
+  // document.write("<br>");
+  console.log("");
+}
+
+// 고수
+// console.log("*");
+// console.log("**");
+// console.log("***");
+// console.log("****");
+// console.log("***");
+// console.log("**");
+// console.log("*");
